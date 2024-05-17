05bb29f1d3f44c7424da80955eb366ac50a9cf90
Zainab Adegoke <zainabolaitan5@gmail.com>
2024-05-17 19:12:01 +0200

    [DFS-4] Added Express setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Express');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
